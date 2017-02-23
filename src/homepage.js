cd82bd5f02c285d7d5c2cae253adb798e7708175
brownty47 <brownty47@gmail.com>
2017-02-22 19:07:36 -0600

    Setting up React App
    organized files and components hierarchically based on mock up, initial
    webpack setup

---
+++ src/homepage.js
@@ -0,0 +1,13 @@
+import autobind from 'autobind-decorator'
+import React from 'react'
+
+import header from './header'
+import hero from './hero'
+import supporting1 from './supporting1'
+import supporting2 from './supporting2'
+import supporting3 from './supporting3'
+import footer from './carfooterousel'
+
+// import css from './app.css'
+
+export default class Home extends React.Component {
---
+++ src/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Home = require('homepage.js');
+
+ReactDOM.render(<Home />, document.getElementById('app'));
